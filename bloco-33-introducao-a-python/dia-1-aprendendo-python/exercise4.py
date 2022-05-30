c90c6b7f77479fc67be4842407a5293fc324b9a9
Matheus Henrique Gonzaga <matheushg@pop-os.localdomain>
2022-05-29 22:36:56 -0300

    feat: exercise4

---
+++ bloco-33-introducao-a-python/dia-1-aprendendo-python/exercise4.py
@@ -0,0 +1,10 @@
+# Crie uma função que receba dois números e retorne o maior deles.
+
+
+def bigger_number(num1, num2):
+    if num1 > num2:
+        return num1
+    return num2
+
+
+print(bigger_number(10, 50))
