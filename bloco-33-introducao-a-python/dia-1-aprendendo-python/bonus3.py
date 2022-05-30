84891959ffd28a40823eb5238cb5cf6742c62ed1
Matheus Henrique Gonzaga <matheushg@pop-os.localdomain>
2022-05-29 23:19:21 -0300

    feat: bonus3

---
+++ bloco-33-introducao-a-python/dia-1-aprendendo-python/bonus3.py
@@ -0,0 +1,23 @@
+# Crie uma função que receba um número inteiro N e
+# retorne o somatório de todos os números de 1 até N.
+# Por exemplo, para N = 5, o valor esperado será 15.
+
+
+def somatorio(n):
+    soma = 0
+    for i in range(1, n + 1):
+        soma += i
+    return soma
+
+
+print(somatorio(5))
+
+
+# Usando a função sum()
+
+
+def somatorio(n):
+    return sum(range(1, n + 1))
+
+
+print(somatorio(5))
